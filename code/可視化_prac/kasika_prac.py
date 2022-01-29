552c585ffe9c7b7821a3d826a939978f3db4b0ad
Daichi <d.sugawara@4nonome.com>
2022-01-29 14:01:42 +0900

    first commit

---
+++ code/可視化_prac/kasika_prac.py
@@ -0,0 +1,17 @@
+# 可視化
+#%%
+import seaborn as sns
+import matplotlib.pyplot as plt
+#%%
+# library & dataset
+df = sns.load_dataset('iris')
+
+# Custom the inside plot: options are: “scatter” | “reg” | “resid” | “kde” | “hex”
+sns.jointplot(x=df["sepal_length"], y=df["sepal_width"], kind='scatter')
+sns.jointplot(x=df["sepal_length"], y=df["sepal_width"], kind='hex')
+sns.jointplot(x=df["sepal_length"], y=df["sepal_width"], kind='kde')
+
+plt.show()
+# %%
+print(sns.get_dataset_names())
+# %%
